cff109a6ec33455c889a909d58d9f61ffcacf7ea
Yogesh <burayogesh78@gmail.com>
2024-10-14 13:01:40 +0530

    changed the directory

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-import express from "express";
+// import express from "express";
 
-import "dotenv/config";
-const app = express();
-const PORT = process.env.PORT || 3004;
+// import "dotenv/config";
+// const app = express();
+// const PORT = process.env.PORT || 3004;
 
-app.use(express.json()); app.use(express.urlencoded({ extended: true }));
+// app.use(express.json()); app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res)=>{
-  res.send("API Working")
-})
-
-
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+// app.get('/', (req,res)=>{
+//   res.send("API Working")
+// })
 
 
 
-// import {createuser, uploadfileserver , singlefile, getfile, deletefile} from  "./Controllers/index.js"
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
 
 
 
-import {createuser} from  "./Controllers/index.js"
-app.post("/create-user", createuser );
-// app.post("/upload-file/:email", singlefile, uploadfileserver );
-// app.get("/get-file/:email", getfile );
-// app.delete("/delete-file/:email/:id", deletefile);
+// // import {createuser, uploadfileserver , singlefile, getfile, deletefile} from  "./Controllers/index.js"
 
 
+
+// import {createuser} from  "./Controllers/index.js"
+// app.post("/create-user", createuser );
+// // app.post("/upload-file/:email", singlefile, uploadfileserver );
+// // app.get("/get-file/:email", getfile );
+// // app.delete("/delete-file/:email/:id", deletefile);
 
 
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running successfully on port ${PORT}`);
-});
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running successfully on port ${PORT}`);
+// });
 
 
